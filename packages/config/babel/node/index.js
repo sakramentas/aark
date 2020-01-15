0f5318c5be55bac902feb030c9f3be37eca23855
Lucas Sacramento <lucas.arki@gmail.com>
2020-01-15 00:54:24 +0000

    Updated babel and eslint config

--- packages/config/babel/node/index.js
+++ packages/config/babel/node/index.js
@@ -2,6 +2,24 @@ module.exports = api => {
   api.cache(true);
 
   return {
-    presets: ['@babel/preset-env'],
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+    ],
+    plugins: [
+      '@babel/plugin-transform-regenerator',
+      [
+        '@babel/plugin-transform-runtime',
+        {
+          regenerator: true,
+        },
+      ],
+    ],
   };
 };
