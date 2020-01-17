c4a93517b88451f831fc7f507565dcfd8e5c76bf
Lucas Sacramento <lucas.arki@gmail.com>
2020-01-17 22:16:39 +0000

    Added prepublish script to packages

--- packages/config/aark-babel-preset/node/index.js
+++ packages/config/aark-babel-preset/node/index.js
@@ -1,6 +1,4 @@
 module.exports = api => {
-  api.cache(true);
-
   return {
     presets: [
       [
--- services/aark-generator-core/index.js
+++ services/aark-generator-core/index.js
@@ -1,6 +1,3 @@
-#!/usr/bin/env node
-require('dotenv').config({});
-
 require('@babel/register')({
   presets: ['@aark/babel-preset/node'],
   include: ['../../node_modules'],
