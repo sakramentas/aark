329af762c720cf936524b56d00dc75b47ce53120
Lucas Sacramento <lucas.arki@gmail.com>
2020-01-28 00:16:11 +0000

    Fixed ESLint lib

--- packages/config/aark-eslint-config/src/index.js
+++ packages/config/aark-eslint-config/src/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: ['airbnb', 'prettier', 'prettier/react', 'plugin:flowtype/recommended'],
-  plugins: ['react', 'prettier', 'flowtype', 'react-hooks'],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['react', 'prettier', 'react-hooks'],
   env: {
     browser: true,
     commonjs: true,
